58649f814eb1986e69a675b8a3054726bc294504
Lankybean17 <69683144+Lankybean17@users.noreply.github.com>
2020-10-12 13:00:30 +0100

    Update
    Index Changes

--- js/modal.js
+++ js/modal.js
@@ -20,3 +20,9 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+  window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
